717ad0ac0d483ad2f1a1f089d0342fe68c5dbe11
Arseny Kapoulkine <arseny.kapoulkine@gmail.com>
2009-07-15 08:06:06 +0000

    lusp: Added deep bytecode dumping (with nested closure code)

--- src/eval.c
+++ src/eval.c
@@ -9,9 +9,11 @@
 #include <lusp/write.h>
 #include <lusp/environment.h>
 
+#include <core/string.h>
+
 #include <stdio.h>
 
-static void dump_bytecode(struct lusp_vm_bytecode_t* code)
+static void dump_bytecode(struct lusp_vm_bytecode_t* code, const char* indent, bool deep)
 {
 	for (unsigned int i = 0; i < code->count; ++i)
 	{
@@ -20,45 +22,56 @@ static void dump_bytecode(struct lusp_vm_bytecode_t* code)
 		switch (op->opcode)
 		{
 		case LUSP_VMOP_GET_OBJECT:
-			printf("%02d: get_object %p [ ", i, op->get_object.object);
+			printf("%s%02d: get_object %p [ ", indent, i, op->get_object.object);
 			lusp_write(op->get_object.object);
 			printf(" ]\n");
+			
+			if (op->get_object.object && op->get_object.object->type == LUSP_OBJECT_CLOSURE && deep)
+			{
+			    char new_indent[256];
+			    
+			    str_copy(new_indent, sizeof(new_indent), indent);
+			    str_concat(new_indent, sizeof(new_indent), "\t");
+			    
+			    dump_bytecode(op->get_object.object->closure.code, new_indent, deep);
+			}
+			
 			break;
 			
 		case LUSP_VMOP_GET_LOCAL:
 		case LUSP_VMOP_SET_LOCAL:
-			printf("%02d: %s_local %d %d\n", i, (op->opcode == LUSP_VMOP_SET_LOCAL) ? "set" : "get",
+			printf("%s%02d: %s_local %d %d\n", indent, i, (op->opcode == LUSP_VMOP_SET_LOCAL) ? "set" : "get",
 				op->getset_local.depth, op->getset_local.index);
 			break;
 			
 		case LUSP_VMOP_GET_GLOBAL:
 		case LUSP_VMOP_SET_GLOBAL:
-			printf("%02d: %s_global %p [ %s ]\n", i, (op->opcode == LUSP_VMOP_SET_GLOBAL) ? "set" : "get",
+			printf("%s%02d: %s_global %p [ %s ]\n", indent, i, (op->opcode == LUSP_VMOP_SET_GLOBAL) ? "set" : "get",
 				op->getset_global.slot, op->getset_global.slot->name);
 			break;
 			
 		case LUSP_VMOP_PUSH:
-			printf("%02d: push\n", i);
+			printf("%s%02d: push\n", indent, i);
 			break;
 			
 		case LUSP_VMOP_BIND:
-			printf("%02d: bind %d\n", i, op->bind.count);
+			printf("%s%02d: bind %d\n", indent, i, op->bind.count);
 			break;
 			
 		case LUSP_VMOP_UNBIND:
-			printf("%02d: unbind\n", i);
+			printf("%s%02d: unbind\n", indent, i);
 			break;
 			
 		case LUSP_VMOP_CALL:
-			printf("%02d: call\n", i);
+			printf("%s%02d: call\n", indent, i);
 			break;
 			
 		case LUSP_VMOP_RETURN:
-			printf("%02d: return\n", i);
+			printf("%s%02d: return\n", indent, i);
 			break;
 			
 		default:
-			printf("%02d: unknown\n", i);
+			printf("%s%02d: unknown\n", indent, i);
 		}
 	}
 }
@@ -69,7 +82,7 @@ struct lusp_object_t* lusp_eval(struct lusp_object_t* object)
 	
 	struct lusp_vm_bytecode_t* code = object->closure.code;
 	
-	dump_bytecode(code);
+	dump_bytecode(code, "", true);
 
 	return 0;
 }
