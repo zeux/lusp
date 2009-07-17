37b3cf4f09825e075e2c05298294c64d68ea1231
Arseny Kapoulkine <arseny.kapoulkine@gmail.com>
2009-07-17 20:08:40 +0000

    lusp: JIT refactoring (separated assembly commands into groups, separated VM commands into functions, added automatic local label handling)

--- src/win/assembler.h
+++ src/win/assembler.h
@@ -2,45 +2,72 @@
 
 #pragma once
 
+#define CODE() code
 #define EMIT8(value) *code++ = value
-#define EMIT32(value) *(unsigned*)code = (unsigned)(uintptr_t)(value), code += 4
+#define EMIT32(value) *(uint32_t*)code = (uint32_t)(uintptr_t)(value), code += sizeof(uint32_t)
 
 #define RET() EMIT8(0xc3)
+
 #define MOV_EAX_IMM32(value) EMIT8(0xb8), EMIT32(value)
+
 #define MOV_ADDR_EAX(addr) EMIT8(0xa3), EMIT32(addr)
+
 #define MOV_EAX_ADDR(addr) EMIT8(0xa1), EMIT32(addr)
 #define MOV_ECX_ADDR(addr) EMIT8(0x3e), EMIT8(0x8b), EMIT8(0x0d), EMIT32(addr)
-#define ADD_ADDR_IMM32(addr, value) EMIT8(0x3e), EMIT8(0x81), EMIT8(0x05), EMIT32(addr), EMIT32(value)
+
 #define MOV_PECX_EBX() EMIT8(0x89), EMIT8(0x19)
+#define MOV_PEDX_EAX() EMIT8(0x89), EMIT8(0x02)
+
 #define MOV_EBX_ECX() EMIT8(0x8b), EMIT8(0xd9)
+
 #define MOV_EBX_PEBX() EMIT8(0x8b), EMIT8(0x1b)
-#define MOV_EAX_PEBX_OFF32(offset) EMIT8(0x8b), EMIT8(0x83), EMIT32(offset)
-#define MOV_PEBX_OFF32_EAX(offset) EMIT8(0x89), EMIT8(0x83), EMIT32(offset)
 #define MOV_ECX_PEBX() EMIT8(0x8b), EMIT8(0x0b)
 #define MOV_ECX_PECX() EMIT8(0x8b), EMIT8(0x09)
+
+#define MOV_EAX_PEBX_OFF32(offset) EMIT8(0x8b), EMIT8(0x83), EMIT32(offset)
 #define MOV_EAX_PECX_OFF32(offset) EMIT8(0x8b), EMIT8(0x81), EMIT32(offset)
-#define MOV_PECX_OFF32_EAX(offset) EMIT8(0x89), EMIT8(0x81), EMIT32(offset)
-#define JMP_IMM32(offset) EMIT8(0xe9), EMIT32(offset)
-#define TEST_EAX_EAX() EMIT8(0x85), EMIT8(0xc0)
-#define JZ_IMM8(offset) EMIT8(0x74), EMIT8(offset)
-#define CMP_PEAX_IMM8(value) EMIT8(0x83), EMIT8(0x38), EMIT8(value)
-#define CMP_PEAX_OFF8_IMM8(offset, value) EMIT8(0x83), EMIT8(0x78), EMIT8(offset), EMIT8(value)
-#define JNE_IMM8(offset) EMIT8(0x75), EMIT8(offset)
-#define JE_IMM32(offset) EMIT8(0x0f), EMIT8(0x84), EMIT32(offset)
-#define MOV_PEAX_IMM32(value) EMIT8(0xc7), EMIT8(0x00), EMIT32(value)
-#define MOV_PEAX_OFF8_EBX(offset) EMIT8(0x89), EMIT8(0x58), EMIT8(offset)
-#define MOV_PEAX_OFF8_IMM32(offset, value) EMIT8(0xc7), EMIT8(0x40), EMIT8(offset), EMIT32(value)
-#define PUSH_IMM32(value) EMIT8(0x68), EMIT32(value)
-#define MOV_EAX_PEAX_OFF8(offset) EMIT8(0x8b), EMIT8(0x40), EMIT8(offset)
-#define CALL_EAX() EMIT8(0xff), EMIT8(0xd0)
-#define PUSH_EBX() EMIT8(0x53)
-#define MOV_EBX_PEAX_OFF8(offset) EMIT8(0x8b), EMIT8(0x58), EMIT8(offset)
-#define POP_EBX() EMIT8(0x5b)
-#define MOV_PEDX_EAX() EMIT8(0x89), EMIT8(0x02)
-#define ADD_EDX_IMM8(value) EMIT8(0x83), EMIT8(0xc2), EMIT8(value)
 #define MOV_ESI_PEDX_OFF32(offset) EMIT8(0x8b), EMIT8(0xb2), EMIT32(offset)
+
+#define MOV_PEBX_OFF32_EAX(offset) EMIT8(0x89), EMIT8(0x83), EMIT32(offset)
+#define MOV_PECX_OFF32_EAX(offset) EMIT8(0x89), EMIT8(0x81), EMIT32(offset)
 #define MOV_PECX_OFF32_ESI(offset) EMIT8(0x89), EMIT8(0xb1), EMIT32(offset)
-#define SUB_EDX_IMM32(value) EMIT8(0x81), EMIT8(0xea), EMIT32(value)
+
+#define MOV_PEAX_IMM32(value) EMIT8(0xc7), EMIT8(0x00), EMIT32(value)
+
+#define MOV_PEAX_OFF8_EBX(offset) EMIT8(0x89), EMIT8(0x58), EMIT8(offset)
+
+#define MOV_PEAX_OFF8_IMM32(offset, value) EMIT8(0xc7), EMIT8(0x40), EMIT8(offset), EMIT32(value)
+
+#define MOV_EAX_PEAX_OFF8(offset) EMIT8(0x8b), EMIT8(0x40), EMIT8(offset)
+#define MOV_EBX_PEAX_OFF8(offset) EMIT8(0x8b), EMIT8(0x58), EMIT8(offset)
+
+#define JMP_IMM32(offset) EMIT8(0xe9), EMIT32(offset)
+
+#define JE_IMM32(offset) EMIT8(0x0f), EMIT8(0x84), EMIT32(offset)
+
+#define JZ_IMM8(label) EMIT8(0x74), label = CODE(), EMIT8(0)
+#define JNE_IMM8(label) EMIT8(0x75), label = CODE(), EMIT8(0)
+
+#define LABEL(label) *(uint8_t*)label = (uint8_t)(uintptr_t)(CODE() - label - 1)
+
+#define TEST_EAX_EAX() EMIT8(0x85), EMIT8(0xc0)
+
+#define CMP_PEAX_IMM8(value) EMIT8(0x83), EMIT8(0x38), EMIT8(value)
+
+#define CMP_PEAX_OFF8_IMM8(offset, value) EMIT8(0x83), EMIT8(0x78), EMIT8(offset), EMIT8(value)
+
+#define CALL_EAX() EMIT8(0xff), EMIT8(0xd0)
+
+#define PUSH_IMM32(value) EMIT8(0x68), EMIT32(value)
+
+#define PUSH_EBX() EMIT8(0x53)
 #define PUSH_EDX() EMIT8(0x52)
+#define POP_EBX() EMIT8(0x5b)
 #define POP_EDX() EMIT8(0x5a)
 #define POP_ECX() EMIT8(0x59)
+
+#define ADD_EDX_IMM8(value) EMIT8(0x83), EMIT8(0xc2), EMIT8(value)
+
+#define ADD_ADDR_IMM32(addr, value) EMIT8(0x3e), EMIT8(0x81), EMIT8(0x05), EMIT32(addr), EMIT32(value)
+
+#define SUB_EDX_IMM32(value) EMIT8(0x81), EMIT8(0xea), EMIT32(value)
--- src/win/jit.c
+++ src/win/jit.c
@@ -26,13 +26,248 @@ void* allocate_code()
 	return result;
 }
 
+// registers:
+// eax: value
+// ebx: bind_frame
+// ecx: used for internal calculations
+// edx: eval_stack
+// esi: used for internal calculations
+
+static inline uint8_t* compile_get_object(uint8_t* code, struct lusp_vm_op_t op)
+{
+	// mov eax, imm32
+	MOV_EAX_IMM32(op.get_object.object);
+	
+	return code;
+}
+
+static inline uint8_t* compile_getset_local(uint8_t* code, struct lusp_vm_op_t op)
+{
+	if (op.getset_local.depth == 0)
+	{
+		// mov eax, dword ptr [ebx + offset]
+		// mov dword ptr [ebx + offset], eax
+		unsigned int offset = op.getset_local.index * 4 + 4;
+		(op.opcode == LUSP_VMOP_GET_LOCAL) ? (MOV_EAX_PEBX_OFF32(offset)) : (MOV_PEBX_OFF32_EAX(offset));
+	}
+	else
+	{
+		// mov ecx, dword ptr [ebx]
+		MOV_ECX_PEBX();
+
+		// mov ecx, dword ptr [ecx]
+		for (unsigned int i = 1; i < op.getset_local.depth; ++i) MOV_ECX_PECX();
+
+		// mov eax, dword ptr [ecx + offset]
+		// mov dword ptr [ecx + offset], eax
+		unsigned int offset = op.getset_local.index * 4 + 4;
+		(op.opcode == LUSP_VMOP_GET_LOCAL) ? (MOV_EAX_PECX_OFF32(offset)) : (MOV_PECX_OFF32_EAX(offset));
+	}
+	
+	return code;
+}
+
+static inline uint8_t* compile_getset_global(uint8_t* code, struct lusp_vm_op_t op)
+{
+	// mov eax, dword ptr [&slot->value]
+	// mov dword ptr [&slot->value], eax
+	struct lusp_object_t** value = &op.getset_global.slot->value;
+	(op.opcode == LUSP_VMOP_GET_GLOBAL) ? (MOV_EAX_ADDR(value)) : (MOV_ADDR_EAX(value));
+	
+	return code;
+}
+
+static inline uint8_t* compile_push(uint8_t* code, struct lusp_vm_op_t op)
+{
+	(void)op;
+	
+	// mov dword ptr [edx], eax
+	MOV_PEDX_EAX();
+
+	// add edx, 4
+	ADD_EDX_IMM8(4);
+	
+	return code;
+}
+
+static inline uint8_t* compile_bind(uint8_t* code, struct lusp_vm_op_t op)
+{
+	// mov ecx, heap.current
+	MOV_ECX_ADDR(&g_lusp_heap.current);
+
+	// add heap.current, 4 + count * 4
+	ADD_ADDR_IMM32(&g_lusp_heap.current, 4 + op.bind.count * 4);
+
+	// mov dword ptr [ecx], ebx
+	MOV_PECX_EBX();
+
+	// sub edx, count * 4
+	SUB_EDX_IMM32(op.bind.count * 4);
+
+	// mov esi, dword ptr [edx + offset]
+	// mov dword ptr [ecx + offset], esi
+	for (unsigned int j = 0; j < op.bind.count; ++j)
+	{
+		MOV_ESI_PEDX_OFF32(j * 4);
+		MOV_PECX_OFF32_ESI(j * 4 + 4);
+	}
+
+	// mov ebx, ecx
+	MOV_EBX_ECX();
+	
+	return code;
+}
+
+static inline uint8_t* compile_unbind(uint8_t* code, struct lusp_vm_op_t op)
+{
+	(void)op;
+	
+	// mov ebx, dword ptr [ebx]
+	MOV_EBX_PEBX();
+	
+	return code;
+}
+
+static inline uint8_t* compile_call(uint8_t* code, struct lusp_vm_op_t op, struct lusp_environment_t* env)
+{
+	// cmp dword ptr [eax], LUSP_OBJECT_PROCEDURE
+	CMP_PEAX_IMM8(LUSP_OBJECT_PROCEDURE);
+
+	// jne closure
+	uint8_t* closure;
+	JNE_IMM8(closure);
+
+	// push count
+	PUSH_IMM32(op.call.count);
+
+	// sub edx, count * 4
+	SUB_EDX_IMM32(op.call.count * 4);
+
+	// push edx
+	PUSH_EDX();
+
+	// push env
+	PUSH_IMM32(env);
+
+	// mov eax, dword ptr [eax + 4]
+	MOV_EAX_PEAX_OFF8(4);
+
+	// call eax
+	CALL_EAX();
+
+	// pop edx
+	POP_EDX();
+
+	// pop edx
+	POP_EDX();
+
+	// pop ecx
+	POP_ECX();
+
+	// jmp end
+	JMP_IMM32(13);
+
+	// closure:
+	LABEL(closure);
+	
+	// push ebx
+	PUSH_EBX();
+
+	// mov ebx, dword ptr [eax + 4]
+	MOV_EBX_PEAX_OFF8(4);
+
+	// mov eax, dword ptr [eax + 8]
+	MOV_EAX_PEAX_OFF8(8);
+
+	// mov eax, dword ptr [eax + 12]
+	MOV_EAX_PEAX_OFF8(12);
+
+	// call eax
+	CALL_EAX();
+
+	// pop ebx
+	POP_EBX();
+
+	// end:
+	return code;
+}
+
+static inline uint8_t* compile_ret(uint8_t* code, struct lusp_vm_op_t op)
+{
+	(void)op;
+	
+	// ret
+	RET();
+	
+	return code;
+}
+
+static inline uint8_t* compile_jump(uint8_t* code, struct lusp_vm_op_t op)
+{
+	(void)op;
+	
+	// jmp offset
+	JMP_IMM32(0);
+	
+	return code;
+}
+
+static inline uint8_t* compile_jump_ifnot(uint8_t* code, struct lusp_vm_op_t op)
+{
+	(void)op;
+	
+	// test eax, eax
+	TEST_EAX_EAX();
+
+	// jz skip
+	uint8_t* skip1;
+	JZ_IMM8(skip1);
+
+	// cmp dword ptr [eax], LUSP_OBJECT_BOOLEAN
+	CMP_PEAX_IMM8(LUSP_OBJECT_BOOLEAN);
+
+	// jne skip
+	uint8_t* skip2;
+	JNE_IMM8(skip2);
+
+	// cmp dword ptr [eax + 4], false
+	CMP_PEAX_OFF8_IMM8(4, 0);
+
+	// je label
+	JE_IMM32(0);
+
+	// skip:
+	LABEL(skip1);
+	LABEL(skip2);
+	
+	return code;
+}
+
+static inline uint8_t* compile_create_closure(uint8_t* code, struct lusp_vm_op_t op)
+{
+	// mov eax, heap.current
+	MOV_EAX_ADDR(&g_lusp_heap.current);
+
+	// add heap.current, sizeof(struct lusp_object_t)
+	ADD_ADDR_IMM32(&g_lusp_heap.current, sizeof(struct lusp_object_t));
+
+	// mov dword ptr [eax], LUSP_OBJECT_CLOSURE
+	MOV_PEAX_IMM32(LUSP_OBJECT_CLOSURE);
+
+	// mov dword ptr [eax + 4], ebx
+	MOV_PEAX_OFF8_EBX(4);
+
+	// mov dword ptr [eax + 8], op.create_closure.code
+	MOV_PEAX_OFF8_IMM32(8, op.create_closure.code);
+
+	// compile bytecode recursively
+	lusp_compile_jit(op.create_closure.code);
+
+	return code;
+}
+
 void compile(unsigned char* code, struct lusp_environment_t* env, struct lusp_vm_op_t* ops, unsigned int count)
 {
-	// registers:
-	// eax: value
-	// ebx: bind_frame
-	// ecx: used for internal calculations
-	
 	unsigned char* labels[1024];
 	unsigned char* jumps[1024];
 	
@@ -48,75 +283,25 @@ void compile(unsigned char* code, struct lusp_environment_t* env, struct lusp_vm
 		switch (op.opcode)
 		{
 		case LUSP_VMOP_GET_OBJECT:
-			// mov eax, imm32
-			MOV_EAX_IMM32(op.get_object.object);
+			code = compile_get_object(code, op);
 			break;
 			
 		case LUSP_VMOP_GET_LOCAL:
 		case LUSP_VMOP_SET_LOCAL:
-			if (op.getset_local.depth == 0)
-			{
-				// mov eax, dword ptr [ebx + offset]
-				// mov dword ptr [ebx + offset], eax
-				unsigned int offset = op.getset_local.index * 4 + 4;
-				(op.opcode == LUSP_VMOP_GET_LOCAL) ? (MOV_EAX_PEBX_OFF32(offset)) : (MOV_PEBX_OFF32_EAX(offset));
-			}
-			else
-			{
-				// mov ecx, dword ptr [ebx]
-				MOV_ECX_PEBX();
-				
-				// mov ecx, dword ptr [ecx]
-				for (unsigned int i = 1; i < op.getset_local.depth; ++i) MOV_ECX_PECX();
-				
-				// mov eax, dword ptr [ecx + offset]
-				// mov dword ptr [ecx + offset], eax
-				unsigned int offset = op.getset_local.index * 4 + 4;
-				(op.opcode == LUSP_VMOP_GET_LOCAL) ? (MOV_EAX_PECX_OFF32(offset)) : (MOV_PECX_OFF32_EAX(offset));
-			}
+			code = compile_getset_local(code, op);
 			break;
 			
 		case LUSP_VMOP_GET_GLOBAL:
-			// mov eax, slot->value
-			MOV_EAX_ADDR(&op.getset_global.slot->value);
-			break;
-			
 		case LUSP_VMOP_SET_GLOBAL:
-			// mov slot->value, eax
-			MOV_ADDR_EAX(&op.getset_global.slot->value);
+			code = compile_getset_global(code, op);
 			break;
 			
 		case LUSP_VMOP_PUSH:
-			// mov dword ptr [edx], eax
-			MOV_PEDX_EAX();
-			
-			// add edx, 4
-			ADD_EDX_IMM8(4);
+			code = compile_push(code, op);
 			break;
 			
 		case LUSP_VMOP_BIND:
-			// mov ecx, heap.current
-			MOV_ECX_ADDR(&g_lusp_heap.current);
-			
-			// add heap.current, 4 + count * 4
-			ADD_ADDR_IMM32(&g_lusp_heap.current, 4 + op.bind.count * 4);
-			
-			// mov dword ptr [ecx], ebx
-			MOV_PECX_EBX();
-			
-			// sub edx, count * 4
-			SUB_EDX_IMM32(op.bind.count * 4);
-			
-			// mov esi, dword ptr [edx + offset]
-			// mov dword ptr [ecx + offset], esi
-			for (unsigned int j = 0; j < op.bind.count; ++j)
-			{
-				MOV_ESI_PEDX_OFF32(j * 4);
-				MOV_PECX_OFF32_ESI(j * 4 + 4);
-			}
-			
-			// mov ebx, ecx
-			MOV_EBX_ECX();
+			code = compile_bind(code, op);
 			break;
 			
 		case LUSP_VMOP_BIND_REST:
@@ -124,123 +309,29 @@ void compile(unsigned char* code, struct lusp_environment_t* env, struct lusp_vm
 			break;
 			
 		case LUSP_VMOP_UNBIND:
-			// mov ebx, dword ptr [ebx]
-			MOV_EBX_PEBX();
+			code = compile_unbind(code, op);
 			break;
 			
 		case LUSP_VMOP_CALL:
-			// cmp dword ptr [eax], LUSP_OBJECT_PROCEDURE
-			CMP_PEAX_IMM8(LUSP_OBJECT_PROCEDURE);
-			
-			// jne closure
-			JNE_IMM8(30);
-			
-			// push count
-			PUSH_IMM32(op.call.count);
-			
-			// sub edx, count * 4
-			SUB_EDX_IMM32(op.call.count * 4);
-			
-			// push edx
-			PUSH_EDX();
-			
-			// push env
-			PUSH_IMM32(env);
-
-			// mov eax, dword ptr [eax + 4]
-			MOV_EAX_PEAX_OFF8(4);
-			
-			// call eax
-			CALL_EAX();
-			
-			// pop edx
-			POP_EDX();
-			
-			// pop edx
-			POP_EDX();
-			
-			// pop ecx
-			POP_ECX();
-			
-			// jmp end
-			JMP_IMM32(13);
-			
-			// closure:
-			// push ebx
-			PUSH_EBX();
-			
-			// mov ebx, dword ptr [eax + 4]
-			MOV_EBX_PEAX_OFF8(4);
-			
-			// mov eax, dword ptr [eax + 8]
-			MOV_EAX_PEAX_OFF8(8);
-			
-			// mov eax, dword ptr [eax + 12]
-			MOV_EAX_PEAX_OFF8(12);
-			
-			// call eax
-			CALL_EAX();
-			
-			// pop ebx
-			POP_EBX();
-			
-			// end:
-			
+			code = compile_call(code, op, env);
 			break;
 			
 		case LUSP_VMOP_RETURN:
-			// ret
-			RET();
+			code = compile_ret(code, op);
 			break;
 			
 		case LUSP_VMOP_JUMP:
-			// jmp offset
-			JMP_IMM32(0);
+			code = compile_jump(code, op);
 			jumps[i] = code - 4;
 			break;
 			
 		case LUSP_VMOP_JUMP_IFNOT:
-			// test eax, eax
-			TEST_EAX_EAX();
-
-			// jz skip
-			JZ_IMM8(15);
-			
-			// cmp dword ptr [eax], LUSP_OBJECT_BOOLEAN
-			CMP_PEAX_IMM8(LUSP_OBJECT_BOOLEAN);
-			
-			// jne skip
-			JNE_IMM8(10);
-			
-			// cmp dword ptr [eax + 4], false
-			CMP_PEAX_OFF8_IMM8(4, 0);
-			
-			// je label
-			JE_IMM32(0);
+			code = compile_jump_ifnot(code, op);
 			jumps[i] = code - 4;
-			
-			// skip:
 			break;
 			
 		case LUSP_VMOP_CREATE_CLOSURE:
-			// mov eax, heap.current
-			MOV_EAX_ADDR(&g_lusp_heap.current);
-
-			// add heap.current, sizeof(struct lusp_object_t)
-			ADD_ADDR_IMM32(&g_lusp_heap.current, sizeof(struct lusp_object_t));
-			
-			// mov dword ptr [eax], LUSP_OBJECT_CLOSURE
-			MOV_PEAX_IMM32(LUSP_OBJECT_CLOSURE);
-			
-			// mov dword ptr [eax + 4], ebx
-			MOV_PEAX_OFF8_EBX(4);
-			
-			// mov dword ptr [eax + 8], op.create_closure.code
-			MOV_PEAX_OFF8_IMM32(8, op.create_closure.code);
-			
-			// compile bytecode recursively
-			lusp_compile_jit(op.create_closure.code);
-			
+			code = compile_create_closure(code, op);
 			break;
 			
 		default:
