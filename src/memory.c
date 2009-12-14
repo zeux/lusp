b00dd670f7fc9acb950c2e5a7590ccd66e703f3b
Arseny Kapoulkine <arseny.kapoulkine@gmail.com>
2009-12-14 23:51:12 +0300

    au: Implemented memory events

--- src/memory.c
+++ src/memory.c
@@ -5,14 +5,17 @@
 #include <lusp/memory.h>
 
 #include <mem/arena.h>
+#include <mem/heap.h>
 
-static struct mem_arena_t g_lusp_arena;
-
-struct mem_arena_t;
+static struct mem_heap_t g_lusp_heap;
 
 bool lusp_memory_init(struct mem_arena_t* arena, size_t heap_size)
 {
-	return mem_arena_create_subarena(&g_lusp_arena, arena, heap_size, 16);
+	void* memory = mem_arena_allocate(arena, heap_size, 16);
+	
+	mem_heap_create_on_buffer(&g_lusp_heap, memory, heap_size, 0, "lusp");
+	
+	return true;
 }
 
 void lusp_memory_term()
@@ -21,15 +24,15 @@ void lusp_memory_term()
 
 void* lusp_memory_allocate(size_t size)
 {
-    return mem_arena_allocate(&g_lusp_arena, size, 4);
+    return mem_heap_allocate(&g_lusp_heap, size, 4);
 }
 
 void lusp_memory_deallocate(void* ptr)
 {
-    (void)ptr;
+    mem_heap_deallocate(&g_lusp_heap, ptr);
 }
 
 size_t lusp_memory_get_size()
 {
-    return g_lusp_arena.size - g_lusp_arena.free_size;
+    return g_lusp_heap.size - g_lusp_heap.free_size;
 }
